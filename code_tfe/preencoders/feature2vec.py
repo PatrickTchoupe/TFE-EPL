ed825dc2cba0c18eb280c7d35993595bca971e28
PatrickTchoupe <patricktchoupe85@gmail.com>
2024-03-01 23:45:53 +0100

    8% de code

--- code_tfe/preencoders/feature2vec.py
+++ code_tfe/preencoders/feature2vec.py
@@ -3,10 +3,17 @@
 
 class Feature2VecPreEncoder(object):
 
-    def __init__(self,):
-        pass
+    def __init__(self,dataset):
+        self.vocab = []
+        self.target = []
+        self.context = []
 
     def fit(self, X):
+        # Avoir une liste de voisins pour chaque element d'une feature
+        # fixer le facteur k, pour la création d'exemple negatifs, le nombre d'epochs 
+        # du nn utilisé et la technique d'optimisation
+        # fenetre contextuelle du modele ( context 4 = 2 gauche / 2 droite)
+        
         pass
 
     def transform(self, X):
